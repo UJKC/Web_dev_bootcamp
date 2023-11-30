8c13d7c9e77795d8c48375bdae7d2ce23d4b7d43
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-30 09:49:26 +0530

    task3 done

--- section17/Dicee Challenge - Starting Files/script.js
+++ section17/Dicee Challenge - Starting Files/script.js
@@ -1,1 +1,6 @@
-console.log("HI")
+console.log("HI");
+var randomnumber1 = 0;
+randomnumber1 = Math.floor(Math.random() * 6) + 1;
+console.log(randomnumber1);
+var randomnumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomnumber2);
