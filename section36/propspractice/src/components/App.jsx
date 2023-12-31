0d88ab2f327a619c438fe8357a99beeb0d7b8f13
Ujwal KC <ujwalkcsps@gmail.com>
2023-12-31 16:48:27 +0530

    map

--- section36/propspractice/src/components/App.jsx
+++ section36/propspractice/src/components/App.jsx
@@ -2,25 +2,20 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+function CreateCard(contacts) {
+  return <Card
+    name={contacts.name}
+    imgURL={contacts.imgURL}
+    phone={contacts.phone}
+    email={contacts.email}
+  />
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card name={contacts[0].name}
-      imgURL={contacts[0].imgURL}
-      phone={contacts[0].phone}
-      email={contacts[0].email}
-      />
-      <Card name={contacts[1].name}
-      imgURL={contacts[1].imgURL}
-      phone={contacts[1].phone}
-      email={contacts[1].email}
-      />
-      <Card name={contacts[2].name}
-      imgURL={contacts[2].imgURL}
-      phone={contacts[2].phone}
-      email={contacts[2].email}
-      />
+      {contacts.map(CreateCard)}
     </div>
   );
 }
